ba61ca480967a98153f17dab14e262176d3c6fd4
Ian LaFlamme <ian.laflamme@libertyits.com>
2020-10-08 17:13:49 -0400

    surpress autocloseable warning... bug

--- vistalink/src/main/java/gov/va/api/lighthouse/vistalink/service/controller/RpcInvoker.java
+++ vistalink/src/main/java/gov/va/api/lighthouse/vistalink/service/controller/RpcInvoker.java
@@ -3,7 +3,11 @@ package gov.va.api.lighthouse.vistalink.service.controller;
 import gov.va.api.lighthouse.vistalink.service.api.RpcDetails;
 import gov.va.api.lighthouse.vistalink.service.api.RpcInvocationResult;
 
+/**
+ * This warning is a bug in the OpenJDK, see
+ * https://bugs.openjdk.java.net/browse/JDK-8155591.
+ */
+@SuppressWarnings("try")
 public interface RpcInvoker extends AutoCloseable {
-
   RpcInvocationResult invoke(RpcDetails rpcDetails);
-}
+}tus
