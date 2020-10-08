00aaf60670dfefb8c3fb490294e6f5a3316b4a27
Ian LaFlamme <ian.laflamme@libertyits.com>
2020-10-08 17:15:18 -0400

    formatting

--- vistalink/src/main/java/gov/va/api/lighthouse/vistalink/service/controller/RpcInvoker.java
+++ vistalink/src/main/java/gov/va/api/lighthouse/vistalink/service/controller/RpcInvoker.java
@@ -3,11 +3,8 @@ package gov.va.api.lighthouse.vistalink.service.controller;
 import gov.va.api.lighthouse.vistalink.service.api.RpcDetails;
 import gov.va.api.lighthouse.vistalink.service.api.RpcInvocationResult;
 
-/**
- * This warning is a bug in the OpenJDK, see
- * https://bugs.openjdk.java.net/browse/JDK-8155591.
- */
+/** This warning is a bug in the OpenJDK, see https://bugs.openjdk.java.net/browse/JDK-8155591. */
 @SuppressWarnings("try")
 public interface RpcInvoker extends AutoCloseable {
   RpcInvocationResult invoke(RpcDetails rpcDetails);
-}tus
+}
