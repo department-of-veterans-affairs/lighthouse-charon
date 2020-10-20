3e01c63b13363782bc1e05cc2cdd6c198d014d10
Bryan Schofield <bryan.schofield@libertyits.com>
2020-10-19 22:54:40 -0400

    parallel RPC execution no longer allows corupt login context

--- vistalink/src/main/java/gov/va/api/lighthouse/vistalink/service/controller/ParallelRpcExecutor.java
+++ vistalink/src/main/java/gov/va/api/lighthouse/vistalink/service/controller/ParallelRpcExecutor.java
@@ -5,8 +5,17 @@ import gov.va.api.lighthouse.vistalink.service.api.RpcRequest;
 import gov.va.api.lighthouse.vistalink.service.api.RpcResponse;
 import gov.va.api.lighthouse.vistalink.service.api.RpcResponse.Status;
 import gov.va.api.lighthouse.vistalink.service.config.ConnectionDetails;
+import io.micrometer.core.instrument.util.NamedThreadFactory;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +28,8 @@ public class ParallelRpcExecutor implements RpcExecutor {
 
   private final RpcInvokerFactory rpcInvokerFactory;
   private final VistaNameResolver vistaNameResolver;
+  private final ExecutorService executor =
+      Executors.newFixedThreadPool(8, new NamedThreadFactory("rpc-exec"));
 
   @Override
   public RpcResponse execute(RpcRequest request) {
@@ -28,10 +39,11 @@ public class ParallelRpcExecutor implements RpcExecutor {
       return response.status(Status.NO_VISTAS_RESOLVED).build();
     }
     response.status(Status.OK);
-    targets.parallelStream()
-        .map(details -> rpcInvokerFactory.create(request.principal(), details))
-        .map(invoker -> safelyInvoke(request, invoker))
-        .forEachOrdered(
+    Map<String, Future<RpcInvocationResult>> futureResults = invokeForEachTarget(request, targets);
+    targets.stream()
+        .map(ConnectionDetails::name)
+        .map(vista -> resultOf(vista, futureResults.get(vista)))
+        .forEach(
             result -> {
               if (result.error().isPresent()) {
                 response.status(Status.FAILED);
@@ -41,7 +53,40 @@ public class ParallelRpcExecutor implements RpcExecutor {
     return response.build();
   }
 
+  private RpcInvocationResult failed(String vista, String message) {
+    return RpcInvocationResult.builder()
+        .vista(vista)
+        .error(Optional.of("Failed to get result: " + message))
+        .build();
+  }
+
+  private Map<String, Future<RpcInvocationResult>> invokeForEachTarget(
+      RpcRequest request, List<ConnectionDetails> targets) {
+    Map<String, Future<RpcInvocationResult>> futures = new HashMap<>(targets.size());
+    for (ConnectionDetails target : targets) {
+      futures.put(
+          target.name(),
+          executor.submit(
+              () -> safelyInvoke(request, rpcInvokerFactory.create(request.principal(), target))));
+    }
+    return futures;
+  }
+
+  private RpcInvocationResult resultOf(String vista, Future<RpcInvocationResult> futureResult) {
+    if (futureResult == null) {
+      return failed(vista, "No result found.");
+    }
+    try {
+      return futureResult.get(30, TimeUnit.SECONDS);
+    } catch (TimeoutException | ExecutionException | InterruptedException e) {
+      log.error("Failed to get result from {}", vista, e);
+      return RpcInvocationResult.builder().vista(vista).error(Optional.of(e.getMessage())).build();
+    }
+  }
+
   private RpcInvocationResult safelyInvoke(RpcRequest request, RpcInvoker invoker) {
+    log.info(
+        "Invoking {} / {} for {}", request.rpc().name(), request.rpc().context(), invoker.vista());
     try {
       return invoker.invoke(request.rpc());
     } catch (Exception e) {
--- vistalink/src/main/java/gov/va/api/lighthouse/vistalink/service/controller/VistalinkRpcInvoker.java
+++ vistalink/src/main/java/gov/va/api/lighthouse/vistalink/service/controller/VistalinkRpcInvoker.java
@@ -14,6 +14,7 @@ import java.io.StringReader;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.login.AppConfigurationEntry;
 import javax.security.auth.login.AppConfigurationEntry.LoginModuleControlFlag;
@@ -24,12 +25,16 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 import lombok.Builder;
 import lombok.SneakyThrows;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@ToString(onlyExplicitlyIncluded = true)
 public class VistalinkRpcInvoker implements RpcInvoker {
 
   private static final JAXBContext JAXB_CONTEXT = createJaxbContext();
+
+  private static final AtomicInteger LOGIN_CONTEXT_ID = new AtomicInteger(0);
 
   private final RpcPrincipal rpcPrincipal;
 
@@ -71,7 +76,7 @@ public class VistalinkRpcInvoker implements RpcInvoker {
     try {
       loginContext.logout();
     } catch (LoginException e) {
-      log.warn("Failed to logout", e);
+      log.warn("{} Failed to logout", this, e);
     }
   }
 
@@ -110,12 +115,17 @@ public class VistalinkRpcInvoker implements RpcInvoker {
             };
           }
         };
-    // TODO: make LoginContext name unique?
-    return new LoginContext("vlx:" + connectionDetails.host(), null, handler, jaasConfiguration);
+
+    return new LoginContext(
+        LOGIN_CONTEXT_ID.incrementAndGet() + ":" + connectionDetails.host(),
+        null,
+        handler,
+        jaasConfiguration);
   }
 
   @SneakyThrows
   private VistaKernelPrincipalImpl createVistaKernelPrincipal() {
+    log.info("{} Logging in", this);
     loginContext.login();
     return VistaKernelPrincipalImpl.getKernelPrincipal(loginContext.getSubject());
   }
@@ -123,10 +133,12 @@ public class VistalinkRpcInvoker implements RpcInvoker {
   /** Invoke an RPC with raw types. */
   @SneakyThrows
   public RpcResponse invoke(RpcRequest request) {
-    return connection.executeRPC(request);
+    synchronized (VistalinkRpcInvoker.class) {
+      log.info("{} Executing RPC {}", this, request.getRpcName());
+      return connection.executeRPC(request);
+    }
   }
 
-  // TODO
   @Override
   @SneakyThrows
   public RpcInvocationResult invoke(RpcDetails rpcDetails) {
@@ -144,19 +156,22 @@ public class VistalinkRpcInvoker implements RpcInvoker {
         vistalinkRequest.getParams().setParam(i + 1, parameter.type(), parameter.value());
       }
       RpcResponse vistalinkResponse = invoke(vistalinkRequest);
-      log.info("Response: " + vistalinkResponse.getRawResponse());
+      log.info("{} Response {} chars", this, vistalinkResponse.getRawResponse().length());
       VistalinkXmlResponse xmlResponse = parse(vistalinkResponse);
-
       return RpcInvocationResult.builder()
           .vista(vista())
           .response(xmlResponse.getResponse().getValue())
           .build();
     } finally {
       log.info(
-          "{} ms for {}", Duration.between(start, Instant.now()).toMillis(), rpcDetails.name());
+          "{} {} ms for {}",
+          this,
+          Duration.between(start, Instant.now()).toMillis(),
+          rpcDetails.name());
     }
   }
 
+  @ToString.Include
   @Override
   public String vista() {
     return connectionDetails.name();
