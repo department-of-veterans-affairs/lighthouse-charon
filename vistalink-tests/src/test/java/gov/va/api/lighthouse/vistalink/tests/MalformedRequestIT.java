665b5aa3b83f14c6b2df28e949cf5b8f1b8ec075
Joshua Hulbert <43861345+jhulbert-va@users.noreply.github.com>
2020-10-28 12:57:21 -0400

    API-2730 malformed requests are now rejected with a 400 response (#14)

---
+++ vistalink-tests/src/test/java/gov/va/api/lighthouse/vistalink/tests/MalformedRequestIT.java
@@ -0,0 +1,30 @@
+package gov.va.api.lighthouse.vistalink.tests;
+
+import static org.junit.Assume.assumeTrue;
+
+import gov.va.api.lighthouse.vistalink.service.api.RpcResponse;
+import java.util.Map;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class MalformedRequestIT {
+
+  @Test
+  @SneakyThrows
+  void requestInvalidBodyResponseWith400() {
+    var systemDefinition = SystemDefinitions.get();
+    assumeTrue(systemDefinition.isVistalinkAvailable());
+    String body = "{\"message\": \"Im a malformed request.\"}";
+    var response =
+        TestClients.vistalink()
+            .post(
+                Map.of("Content-Type", "application/json"),
+                systemDefinition.vistalink().apiPath() + "rpc",
+                body)
+            .expect(400)
+            .expectValid(RpcResponse.class);
+    log.info(response.toString());
+  }
+}
---
+++ vistalink/src/main/java/gov/va/api/lighthouse/vistalink/service/controller/WebExceptionHandler.java
@@ -0,0 +1,37 @@
+package gov.va.api.lighthouse.vistalink.service.controller;
+
+import gov.va.api.health.autoconfig.configuration.JacksonConfig;
+import gov.va.api.lighthouse.vistalink.service.api.RpcResponse;
+import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageConversionException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+@RequestMapping(produces = {"application/json"})
+public class WebExceptionHandler {
+
+  @SneakyThrows
+  private RpcResponse failedResponseFor(String message) {
+    RpcResponse response =
+        RpcResponse.builder()
+            .status(RpcResponse.Status.FAILED)
+            .message(Optional.ofNullable(message))
+            .build();
+    log.error("Response: {}", JacksonConfig.createMapper().writeValueAsString(response));
+    return response;
+  }
+
+  @ExceptionHandler({HttpMessageConversionException.class, InvalidRequest.class})
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public RpcResponse handleBadRequestBody(Exception e, HttpServletRequest request) {
+    return failedResponseFor("Failed to read request body.");
+  }
+}
