efe1620d30b8c2273701db07edd6671a4a14f6d3
ilaflamme-va <43884028+ilaflamme-va@users.noreply.github.com>
2020-11-10 14:01:09 -0500

    Api 2733 bad rpc context handling (#23)

--- vistalink/src/main/java/gov/va/api/lighthouse/vistalink/service/controller/InvalidRequest.java
+++ vistalink/src/main/java/gov/va/api/lighthouse/vistalink/service/controller/InvalidRequest.java
@@ -1,7 +1,7 @@
 package gov.va.api.lighthouse.vistalink.service.controller;
 
 public class InvalidRequest extends IllegalArgumentException {
-  public InvalidRequest(String s) {
-    super(s);
+  public InvalidRequest(String message) {
+    super(message);
   }
 }
