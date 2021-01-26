dff0f6b0650b52359d2f2f639ba96f103e80c513
JJ Abando <jesse.abando@libertyits.com>
2021-01-26 10:42:16 -0500

    DfnMacro implemented; unit test

--- vistalink/src/main/java/gov/va/api/lighthouse/vistalink/service/controller/DfnMacro.java
+++ vistalink/src/main/java/gov/va/api/lighthouse/vistalink/service/controller/DfnMacro.java
@@ -1,20 +1,24 @@
 package gov.va.api.lighthouse.vistalink.service.controller;
 
-// @Slf4j
+import gov.va.med.exception.FoundationsException;
+import gov.va.med.vistalink.rpc.RpcRequestFactory;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class DfnMacro implements Macro {
 
   @Override
   public String evaluate(MacroExecutionContext ctx, String value) {
-    //    try {
-    //      var vistalinkRequest = RpcRequestFactory.getRpcRequest();
-    //      vistalinkRequest.setRpcContext("VAFCTF RPC CALLS");
-    //      vistalinkRequest.setUseProprietaryMessageFormat(true);
-    //      vistalinkRequest.setRpcName("VAFCTFU CONVERT ICN TO DFN");
-    //      vistalinkRequest.getParams().setParam(0, "string", value);
-    //      return ctx.invoke(vistalinkRequest).getResults();
-    //    } catch (FoundationsException e) {
-    //      log.info("Exception: " + e.getMessage());
-    //    }
+    try {
+      var vistalinkRequest = RpcRequestFactory.getRpcRequest();
+      vistalinkRequest.setRpcContext("VAFCTF RPC CALLS");
+      vistalinkRequest.setUseProprietaryMessageFormat(true);
+      vistalinkRequest.setRpcName("VAFCTFU CONVERT ICN TO DFN");
+      vistalinkRequest.getParams().setParam(0, "string", value);
+      return ctx.invoke(vistalinkRequest).getResults();
+    } catch (FoundationsException e) {
+      log.info("Exception: " + e.getMessage());
+    }
     return value;
   }
 
--- vistalink/src/test/java/gov/va/api/lighthouse/vistalink/service/controller/DfnMacroTest.java
+++ vistalink/src/test/java/gov/va/api/lighthouse/vistalink/service/controller/DfnMacroTest.java
@@ -1,7 +1,11 @@
 package gov.va.api.lighthouse.vistalink.service.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+import gov.va.med.vistalink.rpc.RpcRequest;
+import gov.va.med.vistalink.rpc.RpcResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -15,7 +19,15 @@ public class DfnMacroTest {
   @Test
   void checkDfnMacro() {
     var dfn = new DfnMacro();
+    RpcResponse response = new FugaziRpcResponse("mydfn");
+    when(executionContext.invoke(any(RpcRequest.class))).thenReturn(response);
     assertThat(dfn.name()).isEqualTo("dfn"); // Coverage requirement, will remove
-    assertThat(dfn.evaluate(executionContext, "5000000347")).isEqualTo("5000000347");
+    assertThat(dfn.evaluate(executionContext, "myicn")).isEqualTo("mydfn");
+  }
+
+  static class FugaziRpcResponse extends RpcResponse {
+    FugaziRpcResponse(String value) {
+      super(null, null, null, null, value, null);
+    }
   }
 }
