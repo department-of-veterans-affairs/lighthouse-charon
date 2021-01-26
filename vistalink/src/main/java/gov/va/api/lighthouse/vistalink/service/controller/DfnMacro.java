20634e466d3875c049b75f9d939dbe74095f9579
Bryan Schofield <43883561+bschofield-va@users.noreply.github.com>
2021-01-26 13:59:34 -0500

    removed try/catch and fixed possible NPE in string compare for DfnMacro

--- vistalink/src/main/java/gov/va/api/lighthouse/vistalink/service/controller/DfnMacro.java
+++ vistalink/src/main/java/gov/va/api/lighthouse/vistalink/service/controller/DfnMacro.java
@@ -11,21 +11,16 @@ public class DfnMacro implements Macro {
   @SneakyThrows
   @Override
   public String evaluate(MacroExecutionContext ctx, String value) {
-    try {
-      var vistalinkRequest = RpcRequestFactory.getRpcRequest();
-      vistalinkRequest.setRpcContext("VAFCTF RPC CALLS");
-      vistalinkRequest.setUseProprietaryMessageFormat(true);
-      vistalinkRequest.setRpcName("VAFCTFU CONVERT ICN TO DFN");
-      vistalinkRequest.getParams().setParam(0, "string", value);
-      String result = ctx.invoke(vistalinkRequest).getResults();
-      if (result.equals("-1^ICN NOT IN DATABASE")) {
-        throw new IcnNotFound();
-      }
-      return result;
-    } catch (FoundationsException e) {
-      log.error("Macro error in Vistalink: {}", e.getMessage(), e);
-      throw e;
+    var vistalinkRequest = RpcRequestFactory.getRpcRequest();
+    vistalinkRequest.setRpcContext("VAFCTF RPC CALLS");
+    vistalinkRequest.setUseProprietaryMessageFormat(true);
+    vistalinkRequest.setRpcName("VAFCTFU CONVERT ICN TO DFN");
+    vistalinkRequest.getParams().setParam(0, "string", value);
+    String result = ctx.invoke(vistalinkRequest).getResults();
+    if ("-1^ICN NOT IN DATABASE".equals(result)) {
+      throw new IcnNotFound();
     }
+    return result;
   }
 
   @Override
