1e78bc6d1d187badb3421f50a6f98b51b457f4ec
jhulbert <joshua.hulbert@libertyits.com>
2021-02-02 17:34:06 -0500

    Vitals isEmpty test

---
+++ vistalink-models/src/test/java/gov/va/api/lighthouse/vistalink/models/vprgetpatientdata/VitalsTest.java
@@ -0,0 +1,42 @@
+package gov.va.api.lighthouse.vistalink.models.vprgetpatientdata;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import gov.va.api.lighthouse.vistalink.models.CodeAndNameXmlAttribute;
+import gov.va.api.lighthouse.vistalink.models.ValueOnlyXmlAttribute;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class VitalsTest {
+
+  public static Stream<Arguments> isNotEmpty() {
+    return Stream.of(
+        Arguments.of(Vitals.Vital.builder().build(), false),
+        Arguments.of(
+            Vitals.Vital.builder().entered(ValueOnlyXmlAttribute.builder().build()).build(), true),
+        Arguments.of(
+            Vitals.Vital.builder().facility(CodeAndNameXmlAttribute.builder().build()).build(),
+            true),
+        Arguments.of(
+            Vitals.Vital.builder().location(CodeAndNameXmlAttribute.builder().build()).build(),
+            true),
+        Arguments.of(Vitals.Vital.builder().measurements(List.of()).build(), true),
+        Arguments.of(
+            Vitals.Vital.builder()
+                .removed(List.of(ValueOnlyXmlAttribute.builder().build()))
+                .build(),
+            true),
+        Arguments.of(
+            Vitals.Vital.builder().taken(ValueOnlyXmlAttribute.builder().build()).build(), true),
+        Arguments.of(VprGetPatientDataSamples.Response.create().vitals().get(0), true));
+  }
+
+  @ParameterizedTest
+  @MethodSource
+  void isNotEmpty(Vitals.Vital vital, boolean expected) {
+    assertThat(Vitals.isNotEmpty(vital)).isEqualTo(expected);
+  }
+}
