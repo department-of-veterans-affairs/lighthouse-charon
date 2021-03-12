fd9deaa4f430b6de655ebd385f3ada16db2481a4
Bryan Schofield <bryan.schofield@libertyits.com>
2021-03-12 13:28:31 -0500

    login specific exceptions

--- charon/src/main/java/gov/va/api/lighthouse/charon/service/controller/UnrecoverableVistalinkExceptions.java
+++ charon/src/main/java/gov/va/api/lighthouse/charon/service/controller/UnrecoverableVistalinkExceptions.java
@@ -1,19 +1,35 @@
 package gov.va.api.lighthouse.charon.service.controller;
 
+import java.io.Serial;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 final class UnrecoverableVistalinkExceptions {
 
-  static class UnrecoverableVistalinkException extends RuntimeException {
-    UnrecoverableVistalinkException(String message, Throwable cause) {
-      super(message, cause);
+  static class BadRpcContext extends UnrecoverableVistalinkException {
+
+    @Serial private static final long serialVersionUID = 6167518157729577717L;
+
+    BadRpcContext(String rpcContext, Throwable cause) {
+      super(rpcContext, cause);
     }
   }
 
-  static class BadRpcContext extends UnrecoverableVistalinkException {
-    BadRpcContext(String rpcContext, Throwable cause) {
-      super(rpcContext, cause);
+  public static class LoginFailure extends UnrecoverableVistalinkException {
+
+    @Serial private static final long serialVersionUID = 4860029675281471237L;
+
+    public LoginFailure(String message) {
+      super(message, null);
+    }
+  }
+
+  static class UnrecoverableVistalinkException extends RuntimeException {
+
+    @Serial private static final long serialVersionUID = -6893741398933405923L;
+
+    UnrecoverableVistalinkException(String message, Throwable cause) {
+      super(message, cause);
     }
   }
 }
