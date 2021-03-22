56480d67747d9ef2b10d0ac53dd13a491f7d3d70
jhulbert <joshua.hulbert@libertyits.com>
2021-03-22 18:07:00 -0400

    Create Ping Request/Response Model

---
+++ charon-models/src/main/java/gov/va/api/lighthouse/charon/models/xobvtestping/XobvTestPing.java
@@ -0,0 +1,61 @@
+package gov.va.api.lighthouse.charon.models.xobvtestping;
+
+import static java.util.stream.Collectors.toMap;
+
+import gov.va.api.lighthouse.charon.api.RpcDetails;
+import gov.va.api.lighthouse.charon.api.RpcInvocationResult;
+import gov.va.api.lighthouse.charon.models.TypeSafeRpc;
+import gov.va.api.lighthouse.charon.models.TypeSafeRpcRequest;
+import gov.va.api.lighthouse.charon.models.TypeSafeRpcResponse;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(staticName = "create")
+public class XobvTestPing implements TypeSafeRpc<XobvTestPing.Request, XobvTestPing.Response> {
+
+  private static final String RPC_NAME = "XOBV TEST PING";
+
+  private static final String DEFAULT_RPC_CONTEXT = "XOBV VISTALINK TESTER";
+
+  @Override
+  public TypeSafeRpcResponse fromResults(List<RpcInvocationResult> results) {
+    return XobvTestPing.Response.builder()
+        .resultsByStation(
+            results.stream()
+                .filter(result -> result.error().isEmpty())
+                .collect(toMap(RpcInvocationResult::vista, RpcInvocationResult::response)))
+        .build();
+  }
+
+  @Builder
+  public static class Request implements TypeSafeRpcRequest {
+    private Optional<String> context;
+
+    /** Build RpcDetails out of the request. */
+    @Override
+    public RpcDetails asDetails() {
+      return RpcDetails.builder()
+          .context(context().orElse(DEFAULT_RPC_CONTEXT))
+          .name(RPC_NAME)
+          .build();
+    }
+
+    /** Lazy Initializer. */
+    Optional<String> context() {
+      if (context == null) {
+        context = Optional.empty();
+      }
+      return context;
+    }
+  }
+
+  @Data
+  @Builder
+  public static class Response implements TypeSafeRpcResponse {
+    private Map<String, String> resultsByStation;
+  }
+}
