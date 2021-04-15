ecd36e5fb97c6df8dfb597c5e031b3bd91340ea3
Bryan Schofield <bryan.schofield@libertyits.com>
2021-04-15 17:17:39 -0400

    UserDemographicsIT just uses charon api path from system definition now

--- charon-tests/src/test/java/gov/va/api/lighthouse/charon/tests/UserDemographicsIT.java
+++ charon-tests/src/test/java/gov/va/api/lighthouse/charon/tests/UserDemographicsIT.java
@@ -5,19 +5,15 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import gov.va.api.lighthouse.charon.api.RpcPrincipal;
 import java.util.Map;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
 
-@Slf4j
 public class UserDemographicsIT {
 
-  @ParameterizedTest
-  @ValueSource(strings = {"/", "/charon/"})
-  void userDemographicsAreReturnedWithAtLeastDuz(String basePath) {
+  @Test
+  void userDemographicsAreReturnedWithAtLeastDuz() {
     assumeTrue(SystemDefinitions.get().isVistaAvailable(), "Vista is unavailable.");
-    var requestPath = basePath + "internal/user-demographics/{site}";
-    log.info("Running health-check for path: {}", requestPath);
+    var requestPath =
+        SystemDefinitions.get().charon().apiPath() + "internal/user-demographics/{site}";
     var headers = TestClients.headers();
     RpcPrincipal principal = SystemDefinitions.get().avCodePrincipal();
     headers.put("accessCode", principal.accessCode());
@@ -30,12 +26,11 @@ public class UserDemographicsIT {
     assertThat(properties.get("DUZ")).isNotNull();
   }
 
-  @ParameterizedTest
-  @ValueSource(strings = {"/", "/charon/"})
-  void userDemographicsRequiresAccessVerifyCodes(String basePath) {
+  @Test
+  void userDemographicsRequiresAccessVerifyCodes() {
     assumeTrue(SystemDefinitions.get().isVistaAvailable(), "Vista is unavailable.");
-    var requestPath = basePath + "internal/user-demographics/{site}";
-    log.info("Running health-check for path: {}", requestPath);
+    var requestPath =
+        SystemDefinitions.get().charon().apiPath() + "internal/user-demographics/{site}";
     var headers = TestClients.headers();
     headers.remove("accessCode");
     headers.remove("verifyCode");
