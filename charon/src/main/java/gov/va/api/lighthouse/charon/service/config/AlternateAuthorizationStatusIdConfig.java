f44c326ed261cdaa263f9bf0d076ae181f60ac04
ilaflamme <ian.laflamme@libertyits.com>
2021-04-15 16:23:10 -0400

    .

--- charon/src/main/java/gov/va/api/lighthouse/charon/service/config/AlternateAuthorizationStatusIdConfig.java
+++ charon/src/main/java/gov/va/api/lighthouse/charon/service/config/AlternateAuthorizationStatusIdConfig.java
@@ -1,40 +1,40 @@
 package gov.va.api.lighthouse.charon.service.config;
 
+import static gov.va.api.lighthouse.charon.service.controller.AlternateAuthorizationStatusIds.AlternateAuthorizationStatusIdsDisabled;
+import static gov.va.api.lighthouse.charon.service.controller.AlternateAuthorizationStatusIds.AuthorizationId;
+import static gov.va.api.lighthouse.charon.service.controller.AlternateAuthorizationStatusIds.MappedAlternateAuthorizationStatusIds;
+
 import gov.va.api.lighthouse.charon.service.controller.AlternateAuthorizationStatusIds;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static gov.va.api.lighthouse.charon.service.controller.AlternateAuthorizationStatusIds.*;
-
 @Configuration
 @Slf4j
 public class AlternateAuthorizationStatusIdConfig {
 
-    @Bean
-    AlternateAuthorizationStatusIds alternateAuthorizationStatusIds(
-            @Autowired AlternateAuthorizationStatusIdProperties properties
-    ) {
-        if(properties.isEnabled()) {
-            log.info("Alternate Authorization Status IDs are enabled.");
-            return MappedAlternateAuthorizationStatusIds.builder()
-                    .publicToPrivateIds(load(properties.getId()))
-                    .build();
-        }
-        return new AlternateAuthorizationStatusIdsDisabled();
+  @Bean
+  AlternateAuthorizationStatusIds alternateAuthorizationStatusIds(
+      @Autowired AlternateAuthorizationStatusIdProperties properties) {
+    if (properties.isEnabled()) {
+      log.info("Alternate Authorization Status IDs are enabled.");
+      return MappedAlternateAuthorizationStatusIds.builder()
+          .publicToPrivateIds(load(properties.getId()))
+          .build();
     }
+    return new AlternateAuthorizationStatusIdsDisabled();
+  }
 
-    private Map<AuthorizationId, AuthorizationId> load(Map<String, String> rawIds) {
-        Map<AuthorizationId, AuthorizationId> publicToPrivateAuthorizationIds = new HashMap<>();
-        for(String k: rawIds.keySet()) {
-            AuthorizationId publicId = AuthorizationId.of(k);
-            AuthorizationId privateId = AuthorizationId.of(rawIds.get(k));
-            publicToPrivateAuthorizationIds.put(publicId, privateId);
-        }
-        return publicToPrivateAuthorizationIds;
+  private Map<AuthorizationId, AuthorizationId> load(Map<String, String> rawIds) {
+    Map<AuthorizationId, AuthorizationId> publicToPrivateAuthorizationIds = new HashMap<>();
+    for (Map.Entry<String, String> entry : rawIds.entrySet()) {
+      AuthorizationId publicId = AuthorizationId.of(entry.getKey());
+      AuthorizationId privateId = AuthorizationId.of(entry.getValue());
+      publicToPrivateAuthorizationIds.put(publicId, privateId);
     }
+    return publicToPrivateAuthorizationIds;
+  }
 }
--- charon/src/main/java/gov/va/api/lighthouse/charon/service/config/AlternateAuthorizationStatusIdProperties.java
+++ charon/src/main/java/gov/va/api/lighthouse/charon/service/config/AlternateAuthorizationStatusIdProperties.java
@@ -1,5 +1,6 @@
 package gov.va.api.lighthouse.charon.service.config;
 
+import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,8 +9,6 @@ import lombok.experimental.Accessors;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Map;
 
 @Configuration
 @EnableConfigurationProperties
@@ -21,14 +20,9 @@ import java.util.Map;
 @Builder
 public class AlternateAuthorizationStatusIdProperties {
 
-    /**
-     * If enabled, the AlternateAuthorizationStatusIdMajig will inspect incoming ids to swap.
-     */
-    private boolean enabled;
+  /** If enabled, the AlternateAuthorizationStatusIdMajig will inspect incoming ids to swap. */
+  private boolean enabled;
 
-    /**
-     * Id mappings, Map of Public Id pair -> Alternate Id pair.
-     * Ex. "duz123@444 : duz987:222".
-     */
-    private Map<String, String> id;
+  /** Id mappings, Map of Public Id pair -> Alternate Id pair. Ex. "duz123@444 : duz987:222". */
+  private Map<String, String> id;
 }
