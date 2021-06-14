3b72200ef3b3484cbedab37b2988abb9c833f506
Joshua Hulbert <joshua.hulbert@libertyits.com>
2021-06-14 15:22:31 -0400

    Readme for xlsx versions

--- charon-models/src/main/java/gov/va/api/lighthouse/charon/models/iblhsamcmsgetins/InsuranceCompanyFile.java
+++ charon-models/src/main/java/gov/va/api/lighthouse/charon/models/iblhsamcmsgetins/InsuranceCompanyFile.java
@@ -3,11 +3,7 @@ package gov.va.api.lighthouse.charon.models.iblhsamcmsgetins;
 import lombok.Builder;
 import lombok.Data;
 
-/**
- * Java representation of Fileman file #36 in VistA.
- *
- * <p>Currently using insuranceElementList.xlsx version: 179.0
- */
+/** Java representation of Fileman file #36 in VistA. */
 @Data
 @Builder
 public class InsuranceCompanyFile {
