feb42518612d28cff1575fd0073222295a1967b0
Joshua Hulbert <joshua.hulbert@libertyits.com>
2021-06-14 16:22:19 -0400

    Give Entry parser a default nld parser

--- charon-models/src/main/java/gov/va/api/lighthouse/charon/models/EntryParser.java
+++ charon-models/src/main/java/gov/va/api/lighthouse/charon/models/EntryParser.java
@@ -1,7 +1,21 @@
 package gov.va.api.lighthouse.charon.models;
 
+import static java.util.stream.Collectors.toList;
+
+import java.util.Arrays;
+import java.util.List;
+
 public interface EntryParser<E> {
 
   /** Defines how to parse a single entry. */
   E parseLine(String entry);
+
+  /** Parse several entries from a new-line delimited string. */
+  default List<E> parseNewLineDelimited(String nldString) {
+    if (nldString == null) {
+      throw new IllegalArgumentException(
+          "Cannot parse a new line delimited string with value: null");
+    }
+    return Arrays.stream(nldString.split("\\n", -1)).map(this::parseLine).collect(toList());
+  }
 }
--- charon-models/src/main/java/gov/va/api/lighthouse/charon/models/iblhsamcmsgetins/GetInsEntryParser.java
+++ charon-models/src/main/java/gov/va/api/lighthouse/charon/models/iblhsamcmsgetins/GetInsEntryParser.java
@@ -1,11 +1,8 @@
 package gov.va.api.lighthouse.charon.models.iblhsamcmsgetins;
 
-import static java.util.stream.Collectors.toList;
 import static org.apache.commons.lang3.StringUtils.trimToNull;
 
 import gov.va.api.lighthouse.charon.models.EntryParser;
-import java.util.Arrays;
-import java.util.List;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(staticName = "create")
@@ -28,14 +25,5 @@ public class GetInsEntryParser implements EntryParser<GetInsEntry> {
         .externalValueRepresentation(trimToNull(caretSeparated[3]))
         .internalValueRepresentation(trimToNull(caretSeparated[4]))
         .build();
-  }
-
-  /** Parse several entries from a new-line delimited string. */
-  public List<GetInsEntry> parseNewLineDelimited(String nldString) {
-    if (nldString == null) {
-      throw new IllegalArgumentException(
-          "Cannot parse a new line delimited string with value: null");
-    }
-    return Arrays.stream(nldString.split("\\n", -1)).map(this::parseLine).collect(toList());
   }
 }
