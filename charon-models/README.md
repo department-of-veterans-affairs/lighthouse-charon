ad3517a76e15a3657de791f5de9333d08ffca130
Joshua Hulbert <43861345+jhulbert-va@users.noreply.github.com>
2021-06-14 15:30:49 -0400

    IBLHS Get Insurance RPC and Entry Models (#92)

---
+++ charon-models/README.md
@@ -0,0 +1,6 @@
+# charon-models
+
+### IBLHS AMCMS GET INS
+- The Fileman File java models define only the fields that are listed in the insuranceElementList.xlsx located 
+  [here](https://libertyits.sharepoint.com/sites/AMCMS/Shared%20Documents/Forms/AllItems.aspx?csf=1&web=1&e=KiDYxm&cid=039212cd%2D3309%2D4056%2Dbfe1%2De24e6b46e527&RootFolder=%2Fsites%2FAMCMS%2FShared%20Documents%2FWellhive%20NI&FolderCTID=0x0120006BFCB76B4FFEDB4E85308A57BC09B287).
+    - Version: 179.0 
---
+++ charon-models/src/main/java/gov/va/api/lighthouse/charon/models/iblhsamcmsgetins/GetInsEntry.java
@@ -0,0 +1,26 @@
+package gov.va.api.lighthouse.charon.models.iblhsamcmsgetins;
+
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.Value;
+
+/**
+ * Models the fields expected in the caret separated string from the IBLHS AMCMS GET INS RPC.
+ *
+ * <p>Expected string representation:
+ *
+ * <p>File#^Internal Entry Number^Field#^External format value^Internal format value
+ */
+@Value
+@Builder
+public class GetInsEntry {
+  @NonNull String fileNumber;
+
+  @NonNull String ien;
+
+  @NonNull String fieldNumber;
+
+  String externalValueRepresentation;
+
+  String internalValueRepresentation;
+}
